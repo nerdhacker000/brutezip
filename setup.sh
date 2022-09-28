7d0b7381ca615067a3edf920a0883a3a89c58b34
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-28 21:50:07 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+clear
+echo "	Welcome to BRUTEZIP tool"
+echo "	Get more hacking tools and tutorials \n	  from my website : www.nerdhaka.blogspot.com
+echo "	Start your attack :)"
+clear
+python brutezip.py
