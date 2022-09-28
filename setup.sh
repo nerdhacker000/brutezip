209dc140d3a32db5f5707b53bf9c83a9d18232fb
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-28 22:03:54 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 clear
+pip install colorama
 echo "	Welcome to BRUTEZIP tool"
 echo "	Get more hacking tools and tutorials \n	  from my website : www.nerdhaka.blogspot.com
 echo "	Start your attack :)"
