424cba2c20b85d1dce92c8dfeeeed9eddbad3922
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-29 10:34:26 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 clear
 PS1="\033[96m(Option)=\033[0m"
+pkg install python
 echo "	Welcome to BRUTEZIP tool"
 echo "	Get more hacking tools and tutorials \n	  from my website : www.nerdhaka.blogspot.com
 echo "	Start your attack :)"
