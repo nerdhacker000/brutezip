5b50c264651eb6111f2459a7a5ac93b411dbda6f
nerdhaka <114361549+nerdhaka@users.noreply.github.com>
2022-09-29 20:33:56 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 clear
-PS0='result:\n'
-PS1="\033[96mbrutezip: \033[0m"
+PS0='results:\n'
+$PS1="\[\e[0;96m\]command\[\e[0m\] \[\e[0;97m\]\$\[\e[0m\]"
 echo "Type \033[96m Type enter to start:[0m"
 read useri
 python brutezip.py
