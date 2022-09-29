c7396cfb2ac940511576635c5c986ea867c25cd8
nerdhacker000 <114361549+nerdhacker000@users.noreply.github.com>
2022-09-29 20:06:26 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 clear
 PS0='result:\n'
 PS1="\033[96mbrutezip: \033[0m"
-PS1="\033[96m(Option)=\033[0m"
 pkg install python
 echo "	Welcome to BRUTEZIP tool"
 echo "	Get more hacking tools and tutorials \n	  from my website : www.nerdhaka.blogspot.com
