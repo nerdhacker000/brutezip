5dee4bd2065cecf2521bc8dcf14cc3386078ecf7
nerdhaka <114361549+nerdhaka@users.noreply.github.com>
2022-09-29 20:59:39 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ PS0='results:\n'
 $PS1="\[\e[0;96m\]command\[\e[0m\] \[\e[0;97m\]\$\[\e[0m\]"
 pkg install python
 echo "	Welcome to BRUTEZIP tool"
-echo "	Get more hacking tools and tutorials \n	  from my website : www.nerdhaka.blogspot.com
+echo "  Get more hacking tools and tutorials..." 
+echo "  from my website : www.nerdhaka.blogspot.com
 echo "	Start your attack :)"
 clear
 sh start.sh
