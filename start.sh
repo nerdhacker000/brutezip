b27725dfb0fa839ec33ed7331f320eafb428c698
nerdhaka <114361549+nerdhaka@users.noreply.github.com>
2022-09-30 08:56:28 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!bin/bash
 clear
+echo "	▉▒▒▒╮ 	 ▉▒▒▓╮	 ╮       ╭ ▉▒▒▒▒▒▓ ▉▒▒▒▒▓  ▉▒▒▒▒▓╮   ▉  ▉▒▒▒▒▓╮"
+echo "	▉    ▓	 ▉    ▓  ▉      ▉     ▓     ▉             ╭▒	 ▉  ▉      ▓" 
+echo "	▉▒▒▒▒╯╮ ▉▒▒▒▒╮ ▉      ▉     ▓     ▉▒▒▒▒▓     ╭▒	 ▉  ▉▒▒▒▒▓╯"
+echo "	▉      ▓ ▉     ▓ ▉	▉      ▓     ▉        ╭▒	 ▉  ▉"
+echo " 	▉▒▒▒▒▒╯ ▉     ▓  ╰▒▒▒╯      ▓     ▉▒▒▒▒▓  ▉▒▒▒▒▒▓	 ▉  ▉"
 PS0='results:\n'
-$PS1="\[\e[0;96m\]command\[\e[0m\] \[\e[0;97m\]\$\[\e[0m\]"
+PS1="\[\e[0;96m\]command\[\e[0m\] \[\e[0;97m\]\$\[\e[0m\]"
 echo "Press enter to start:"
 read useri
 python brutezip.py
